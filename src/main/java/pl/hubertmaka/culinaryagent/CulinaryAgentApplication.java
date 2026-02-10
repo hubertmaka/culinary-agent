ca08a893ae5526f2652aa87cd751526af99f11ec
hubertmaka <hubert.maka02@gmail.com>
2026-02-10 21:14:53 +0100

    CA-7 Init Project Structure

---
+++ src/main/java/pl/hubertmaka/culinaryagent/CulinaryAgentApplication.java
@@ -0,0 +1,13 @@
+package pl.hubertmaka.culinaryagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulinaryAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CulinaryAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/hubertmaka/culinaryagent/CulinaryAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hubertmaka.culinaryagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulinaryAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
